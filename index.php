f86ee42663c30769d82cf3604cc28e3eb13c272b
Fahlander <liam.fahlander@gmail.com>
2023-01-17 08:48:06 +0100

    good code

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+</style>
+</head>
+<body>
+
+<?php
+$string = "Banan";
+for ($i = 1; $i <= 10; $i++) {
+    echo $string . " " . $i . "<br>";
+}
+?>
+
+</body>
+</html>
