1ff7c3fd8841826243f7c12d37c288f5bf95671d
Fahlander <liam.fahlander@gmail.com>
2023-01-17 08:59:30 +0100

    done code

--- index.php
+++ index.php
@@ -2,17 +2,31 @@
 <html>
 <head>
 <style>
+    h1{
+        color: darkslateblue;
+        font-size: xx-large;
+        text-align: center;
+    }
 
+    p{
+        color: darkgreen;
+        font-size: large;
+        text-align: center;
+    }
 </style>
 </head>
 <body>
 
+<h1>Här de komma strängar</h1>
+
+<p>
 <?php
-$string = "Banan";
+$string ="Banan";
 for ($i = 1; $i <= 10; $i++) {
     echo $string . " " . $i . "<br>";
 }
 ?>
+</p>
 
 </body>
 </html>
